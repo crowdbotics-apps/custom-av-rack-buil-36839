cf8d5f1d223fc063d7962610ad67c89072708a9f
Crowdbotics <team@crowdbotics.com>
2022-08-19 12:38:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/custom_av_rack_buil_36839/MainActivity.java
@@ -0,0 +1,15 @@
+package com.custom_av_rack_buil_36839;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "custom_av_rack_buil_36839";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "custom_av_rack_buil_36839",
+  url: "https://custom_av_rack_buil_36839.botics.co",
+  api: "https://custom_av_rack_buil_36839.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
